244f4e33e0175c7474ee542897fe05cd9d630551
Jeffrey Hu <golettoo@gmail.com>
2012-02-02 21:49:57 +0800

    update

--- webapp/apps/bookmark/api/handlers.py
+++ webapp/apps/bookmark/api/handlers.py
@@ -1,10 +1,13 @@
 from piston.handler import BaseHandler, AnonymousBaseHandler
+from urlparse import urlparse
 from bookmark.models import Bookmark
 from django.core.exceptions import ObjectDoesNotExist
 from piston.utils import rc, validate
 from django.db.models.aggregates import Count
 from bookmark.forms import BookmarkForm
+from django.utils.http import urlquote
 from taggit.models import Tag
+from webapp import settings
 
 class BookmarkHandler(BaseHandler):
     model = Bookmark
@@ -133,7 +136,6 @@ class AnonymousBookmarkHandler(BookmarkHandler, AnonymousBaseHandler):
         Return a bookmark if 'bookmark_id' is given,
         otherwise a subset.
         """
-
         bookmarks = Bookmark.objects
 
         if bookmark_id:
@@ -143,13 +145,46 @@ class AnonymousBookmarkHandler(BookmarkHandler, AnonymousBaseHandler):
                 return rc.NOT_FOUND
 
         else:
+            # Get mine data or all data
+            mine = request.GET.get('mine')
+            if mine:
+                relative_url = urlparse(request.META.get('HTTP_REFERER', '/')).path
+                path = urlquote(relative_url)
+                tup = settings.LOGIN_URL, 'next', path
+                json = {'redirect': '%s?%s=%s' % tup}
+                return json
+
             bookmarks = bookmarks.filter(public = True).annotate(num_favorite_users=Count('favorite_users'))
+
+            # Get tag
+            tag_name = request.GET.get('tag')
+            if tag_name:
+                tag = Tag.objects.get(name=tag_name)
+                bookmarks = bookmarks.filter(tags__name__in=[tag.name])
+
+            # Get total records.
             total = bookmarks.count()
 
+            # Get order by, and save into session or get from session
+            order_by = request.GET.get('order_by')
+            if order_by:
+                request.session['order_by'] = order_by
+            else:
+                order_by = request.session.get('order_by', 'newest')
+            if order_by not in ['newest', 'hottest']:
+                order_by = 'newest'
+            if order_by == 'newest':
+                order_by = '-create_time'
+            else:
+                order_by = '-num_favorite_users'
+            bookmarks = bookmarks.order_by(order_by)
+
+            # Get items_per_page
             items_per_page = int(request.GET.get('items_per_page', 10))
-            if items_per_page not in [1, 10, 20, 30]:
+            if items_per_page not in [1, 10, 30, 50]:
                 items_per_page = 10
 
+            # Get current_page
             current_page = int(request.GET.get('current_page', 1))
             if current_page <= 0:
                 current_page = 1
